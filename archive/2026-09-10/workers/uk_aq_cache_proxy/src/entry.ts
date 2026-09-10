3ff6b3b33a2b94a0cff1615e95e257f79a3288cf
mikehinford <mikehinford@hotmail.com>
2026-09-10 15:36:18 +0100

    update cache for media api

--- archive/2026-09-10/workers/uk_aq_cache_proxy/src/entry.ts
+++ archive/2026-09-10/workers/uk_aq_cache_proxy/src/entry.ts
@@ -1,16 +1,11 @@
 import cacheProxy, { type Env as CacheProxyEnv } from "./index.ts";
 import {
-  handleMediaPublicRequest,
-  MEDIA_PUBLIC_API_PATH,
-  type MediaPublicRouteEnv,
-} from "./media_public_route.ts";
-import {
   handleWhoSummaryProxyRequest,
   WHO_SUMMARY_API_PATH,
   type WhoSummaryProxyEnv,
 } from "./who_summary_route.ts";
 
-type Env = CacheProxyEnv & MediaPublicRouteEnv & WhoSummaryProxyEnv;
+type Env = CacheProxyEnv & WhoSummaryProxyEnv;
 
 type ExecutionContext = {
   waitUntil(promise: Promise<unknown>): void;
@@ -19,9 +14,6 @@ type ExecutionContext = {
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
-    if (url.pathname === MEDIA_PUBLIC_API_PATH) {
-      return handleMediaPublicRequest(request, env);
-    }
     if (url.pathname === WHO_SUMMARY_API_PATH) {
       return handleWhoSummaryProxyRequest(request, env, ctx);
     }
